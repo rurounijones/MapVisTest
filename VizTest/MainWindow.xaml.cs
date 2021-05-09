75d6ef341103466f066d981552f089ed9b385c99
Jeffrey Jones <jeff@jones.be>
2021-05-09 15:05:31 +0900

    Prototyping

---
+++ VizTest/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+using MapControl;
+using MapControl.Caching;
+
+namespace VizTest
+{
+    public partial class MainWindow : Window
+    {
+        static MainWindow()
+        {
+            try
+            {
+                ImageLoader.HttpClient.DefaultRequestHeaders.Add("User-Agent", "OverlordBot Visualizer");
+
+                TileImageLoader.Cache = new ImageFileCache(TileImageLoader.DefaultCacheFolder);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (TileImageLoader.Cache is ImageFileCache cache)
+            {
+                Loaded += async (s, e) =>
+                {
+                    await Task.Delay(2000);
+                    await cache.Clean();
+                };
+            }
+        }
+    }
+}
---
+++ VizTest/ViewModels/MapViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Timers;
+using System.Windows;
+using MapControl;
+
+namespace VizTest.ViewModels
+{
+    public class MapViewModel: INotifyPropertyChanged
+    {
+        private static Timer aTimer;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MapTileLayer Osm = new MapTileLayer
+        {
+            TileSource = new TileSource {UriFormat = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"},
+            SourceName = "OpenStreetMap",
+            Description = "© [OpenStreetMap contributors](http://www.openstreetmap.org/copyright)"
+        };
+
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private Location mapCenter = new Location(43, 41);
+        public Location MapCenter
+        {
+            get => mapCenter;
+            set
+            {
+                mapCenter = value;
+                RaisePropertyChanged(nameof(MapCenter));
+            }
+        }
+        
+        public UIElement CurrentMapLayer => Osm;
+
+
+        public ObservableCollection<AirUnit> AirUnits { get; } = new ObservableCollection<AirUnit>();
+
+        public MapViewModel()
+        {
+            AirUnits.Add(new AirUnit
+            {
+                Name = "DOLT-12 | RurouniJones",
+                Location = new Location(44, 42)
+            });
+            // Create a timer with a two second interval.
+            aTimer = new Timer(200);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            AirUnits[0].Location.Latitude -= 0.01;
+        }
+    }
+
+    public class AirUnit : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+            
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                RaisePropertyChanged(nameof(Name));
+            }
+        }
+
+        private Location location;
+        public Location Location
+        {
+            get { return location; }
+            set
+            {
+                location = value;
+                RaisePropertyChanged(nameof(Location));
+            }
+        }
+    }
+}
